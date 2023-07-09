234041a1795e24aac51eb0a80003d19c639d6203
kartikeya <honeychoudhary9797@gmail.com>
2023-07-09 21:27:07 +0530

    kya ho gaya

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages'
+    'storages',
+    
 ]
 
 MIDDLEWARE = [
@@ -131,9 +132,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS =[
-#     os.path.join(BASE_DIR,'static')
-# ]
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static')
+]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
