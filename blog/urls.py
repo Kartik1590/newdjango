340eaa8bd6916887a8cdf3fcbce99b37c3660d12
kartikeya <honeychoudhary9797@gmail.com>
2023-07-09 21:31:55 +0530

    kya ho gaya

--- blog/urls.py
+++ blog/urls.py
@@ -16,5 +16,5 @@ urlpatterns=[
     
 
 ]
-
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
